756484abf4e1671431a07b9ab2e3d9b5be6f0a74
muhammadshiddiq.2021 <muhammadshiddiq.2021@student.uny.ac.id>
2024-09-04 01:54:24 +0000

    menambahkan section about dan memodifikasi navbar

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -8,14 +8,14 @@ const About = () =>{
     return(
         <div className="about">
             <div className="about-left">
-                <img src={about_img} alt="" className="about_img"/>
-                <img src={play_icon} alt="" className="about_img"/>
+                <img src={about_img} alt="" className="about-img"/>
+                <img src={play_icon} alt="" className="play-icon"/>
             </div>
             <div className="about-right">
                 <h3>About University</h3>
                 <h2>Nurturing Tomorrow's Leaders Today</h2>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, consectetur dolores aut quam dolorum laudantium cupiditate! Placeat molestias iste, eveniet aspernatur at in quibusdam ad? Sit nostrum illum dolorem sequi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis libero, possimus laboriosam at quia, tempora illo officiis ut incidunt esse, mam sunt amet a recusandae ipsum nisi officia!</p>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident quisquam distinctio voluptatum quae similique, voluptas doloribus fugiat fuga minima, natus at officiis repellendus quis eius suscipit cupiditate consectetur quam eligendi. </p>
+                <p>Lorem, ipsum dolor sit sectetur niet aspernatur at in quibusdam ad? Sit nostrum illum dolorem sequi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis libero, possimus laboriosam at quia, tempora illo officiis ut incidunt esse, mam sunt amet a recusandae ipsum nisi officia!</p>
+                <p>Lorem ipsum dolor sit, amet consecto voluptatum quae similique, voluptas doloribus fugiat fuga minima, natus at officiis repellendus quis eius suscipit cupiditate consectetur quam eligendi. </p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus cupiditate aut placeat, eos praesentium tempore facilis quia ratione magnam tempora! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam mollitia explicabo iusto illum consectetur velit eligendi earum, accusamus, deleniti</p>
             </div>
         </div>
